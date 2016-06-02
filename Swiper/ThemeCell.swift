7b64ebb01b40c1113e7bbc5d5707e838608b93e2
Matt Brenneke <brenneke@gmail.com>
2016-06-01 22:23:56 -0500

    more with the making of the themes happening

---
+++ Swiper/ThemeCell.swift
@@ -0,0 +1,14 @@
+//
+//  ThemeCell.swift
+//  Swiper
+//
+//  Created by Matt Brenneke on 6/1/16.
+//  Copyright © 2016 Matt Brenneke. All rights reserved.
+//
+
+import UIKit
+
+class ThemeCell : UICollectionViewCell {
+    @IBOutlet var title : UILabel?
+    @IBOutlet var preview : UIImageView?
+}
